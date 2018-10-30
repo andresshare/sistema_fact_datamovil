28a5b390619bd8234219d3834dc9ea24e4ac5ff6
andresshare <andresshare@gmail.com>
2018-10-30 09:16:23 -0500

    modified categorias

--- catalogos/views.py
+++ catalogos/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+
 from catalogos.models import Categoria
+from catalogos.forms import CategoriaForm
 
 
 class CategoriaView(generic.ListView):
@@ -8,4 +11,11 @@ class CategoriaView(generic.ListView):
     template_name = 'categoria_list.html'
     context_object_name = 'obj'
 
+
+class CategoriaNew(generic.CreateView):
+    model=Categoria
+    template_name = 'catalogos/categoria_form.html'
+    context_object_name = 'obj'
+    form_class=CategoriaForm
+    success_url = reverse_lazy('catalogos:categoria_list')
 
