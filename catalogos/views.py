76e79ce050438dcd3ff606b36ecf4f2dfaeba19f
andresshare <andresshare@gmail.com>
2018-10-30 18:54:21 -0500

    auth_ templates

--- catalogos/views.py
+++ catalogos/views.py
@@ -5,34 +5,49 @@ from django.views import generic
 from catalogos.models import Categoria
 from catalogos.forms import CategoriaForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-class CategoriaView(generic.ListView):
+from front.views import SinPrivilegios
+
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+class CategoriaView(LoginRequiredMixin, generic.ListView):
     model = Categoria
-    template_name = 'categoria_list.html'
+    template_name = 'catalogos/categoria_list.htmll'
     context_object_name = 'obj'
+    login_url = 'front:login'
 
 
-class CategoriaNew(generic.CreateView):
+class CategoriaNew(SuccessMessageMixin, LoginRequiredMixin, SinPrivilegios,
+                   generic.CreateView):
+    permission_required = 'catalogos.add_categoria'
     model=Categoria
     template_name = "catalogos/categoria_form.html"
     context_object_name = 'obj'
     form_class=CategoriaForm
     success_url = reverse_lazy("catalogos:categoria_list")
+    login_url = 'front:login'
 
 
-class CategoriaEdit(generic.UpdateView):
+class CategoriaEdit(LoginRequiredMixin,SinPrivilegios,
+                    generic.UpdateView):
+    permission_required = 'catalogos.change_categoria'
     model=Categoria
     template_name = "catalogos/categoria_form.html"
     context_object_name = 'obj'
     form_class=CategoriaForm
     success_url = reverse_lazy("catalogos:categoria_list")
+    login_url = 'front:login'
 
-class CategoriaDel(generic.DeleteView):
+
+class CategoriaDel(LoginRequiredMixin, SinPrivilegios, generic.DeleteView):
+    permission_required = 'catalogos.delete_categoria'
     model = Categoria
     template_name = "catalogos/catalogos_del.html"
     context_object_name = 'obj'
     success_url = reverse_lazy("catalogos:categoria_list")
-
+    login_url = 'front:login'
 
 
 
--- front/views.py
+++ front/views.py
@@ -1,12 +1,33 @@
-from django.views import generic
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.views import generic
 
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+
+from django.urls import reverse_lazy
 
 class HomePage(generic.View):
     def get(self,request,*args, **kwargs):
         return HttpResponse('Home page')
 
 
-class Home(generic.TemplateView):
+#Validacion si el usuario esta autenticado con mixin de  left to the right
+class Home( LoginRequiredMixin, generic.TemplateView):
     template_name = 'front/home.html'
+
+    #Si el usuario no esta autenticado se redirija al login
+    login_url = 'front:login'
+
+class HomeSinPrivilegios(generic.TemplateView):
+    template_name = 'front/sin_privilegios.html'
+
+
+class SinPrivilegios(PermissionRequiredMixin):
+    login_url = 'front:sin_privilegios'
+    raise_exception = False
+    redirect_field_name = 'redirect_to'
+
+    def handle_no_permission(self):
+        return HttpResponseRedirect(reverse_lazy(self.login_url))
+
+
