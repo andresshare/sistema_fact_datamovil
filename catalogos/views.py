429ce99fc66cae00f6e55d15d43fc7c1dcec8637
andresshare <andresshare@gmail.com>
2018-10-29 23:00:10 -0500

    modified menu and views

--- catalogos/views.py
+++ catalogos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from catalogos.models import Categoria
 
+
 class CategoriaView(generic.ListView):
     model = Categoria
     template_name = 'categoria_list.html'
