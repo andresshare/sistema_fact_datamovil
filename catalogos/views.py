851faa5bb9f44c2472c53452d8a25b5344ea1fb7
andresshare <andresshare@gmail.com>
2018-11-01 21:39:06 -0500

    modified templates, categoria form ,migratios

--- catalogos/views.py
+++ catalogos/views.py
@@ -2,74 +2,107 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from catalogos.models import Categoria
-from catalogos.forms import CategoriaForm
+from django.contrib.messages.views import SuccessMessageMixin
 
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
+from catalogos.models import Categoria, SubCategoria, Producto
+from catalogos.forms import CategoriaForm, SubCategoriaForm, ProductoForm
 from front.views import SinPrivilegios
-
-from django.contrib.messages.views import SuccessMessageMixin
-
-#mensaje
-
-from django.contrib.messages.views import SuccessMessageMixin
-
 
 class CategoriaView(LoginRequiredMixin, generic.ListView):
     model = Categoria
-    template_name = 'catalogos/categoria_list.html'
-    context_object_name = 'obj'
+    template_name = "catalogos/categoria_list.html"
+    context_object_name = "obj"
     login_url = 'front:login'
 
 
 class CategoriaNew(SuccessMessageMixin, LoginRequiredMixin, SinPrivilegios,
                    generic.CreateView):
-    permission_required = 'catalogos.add_categoria'
+    permission_required = "catalogos.add_categoria"
     model=Categoria
-    template_name = "catalogos/categoria_form.html"
+    template_name="catalogos/categoria_form.html"
     context_object_name = 'obj'
     form_class=CategoriaForm
-    success_url = reverse_lazy("catalogos:categoria_list")
-    success_message = "Categoría Creada Satisfactoriamente"
+    success_url= reverse_lazy("catalogos:categoria_list")
+    success_message="Categoría Creada Satisfactoriamente"
 
 
-
-class CategoriaEdit(LoginRequiredMixin,SinPrivilegios,
-                    generic.UpdateView):
-    permission_required = 'catalogos.change_categoria'
+class CategoriaEdit(LoginRequiredMixin, SinPrivilegios, generic.UpdateView):
+    permission_required = "catalogos.change_categoria"
     model=Categoria
-    template_name = "catalogos/categoria_form.html"
+    template_name="catalogos/categoria_form.html"
     context_object_name = 'obj'
     form_class=CategoriaForm
-    success_url = reverse_lazy("catalogos:categoria_list")
-
+    success_url= reverse_lazy("catalogos:categoria_list")
 
 
 class CategoriaDel(LoginRequiredMixin, SinPrivilegios, generic.DeleteView):
-    permission_required = 'catalogos.delete_categoria'
-    model = Categoria
-    template_name = "catalogos/catalogos_del.html"
+    permission_required = "catalogos.delete_categoria"
+    model=Categoria
+    template_name="catalogos/catalogos_del.html"
     context_object_name = 'obj'
-    success_url = reverse_lazy("catalogos:categoria_list")
+    success_url= reverse_lazy("catalogos:categoria_list")
+    
+
+class SubCategoriaView(LoginRequiredMixin, generic.ListView):
+    model = SubCategoria
+    template_name = "catalogos/subcategoria_list.html"
+    context_object_name = "obj"
+    login_url = 'front:login'
 
 
+class SubCategoriaNew(SuccessMessageMixin, LoginRequiredMixin, SinPrivilegios,
+                   generic.CreateView):
+    permission_required = "catalogos.add_subcategoria"
+    model=SubCategoria
+    template_name="catalogos/subcategoria_form.html"
+    context_object_name = 'obj'
+    form_class=SubCategoriaForm
+    success_url= reverse_lazy("catalogos:subcategoria_list")
+    success_message="Sub Categoría Creada Satisfactoriamente"
+
+ 
+class SubCategoriaEdit(SuccessMessageMixin, SinPrivilegios, generic.UpdateView):
+    permission_required = "catalogos.change_subcategoria"
+    model = SubCategoria
+    template_name = "catalogos/subcategoria_form.html"
+    context_object_name = 'obj'
+    form_class = SubCategoriaForm
+    success_url = reverse_lazy("catalogos:subcategoria_list")
+    success_message="Sub Categoría Actualizada Satisfactoriamente"
+
+class SubCategoriaDel(LoginRequiredMixin, SinPrivilegios, generic.DeleteView):
+    permission_required = "catalogos.delete_subcategoria"
+    model=SubCategoria
+    template_name="catalogos/catalogos_del.html"
+    context_object_name = 'obj'
+    success_url= reverse_lazy("catalogos:subcategoria_list")
 
 
+class ProductoView(LoginRequiredMixin, generic.ListView):
+    model = Producto
+    template_name = "catalogos/producto_list.html"
+    context_object_name = "obj"
+    login_url = 'front:login'
 
 
+class ProductoNew(SuccessMessageMixin, LoginRequiredMixin, SinPrivilegios,
+                   generic.CreateView):
+    permission_required = "catalogos.add_produco"
+    model=Producto
+    template_name="catalogos/producto_form.html"
+    context_object_name = 'obj'
+    form_class=ProductoForm
+    success_url= reverse_lazy("catalogos:producto_list")
+    success_message="Producto Creado Satisfactoriamente"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+class ProductoEdit(SuccessMessageMixin, SinPrivilegios, generic.UpdateView):
+    permission_required = "catalogos.change_producto"
+    model = Producto
+    template_name = "catalogos/producto_form.html"
+    context_object_name = 'obj'
+    form_class = ProductoForm
+    success_url = reverse_lazy("catalogos:producto_list")
+    success_message="Producto Modificado Satisfactoriamente"
